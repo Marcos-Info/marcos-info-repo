143261a4809f5f42aa27be4f3b6d4c2a417fb209
Marcos-Info <62522663+Marcos-Info@users.noreply.github.com>
2020-03-22 19:17:23 -0300

    Add files via upload

---
+++ jCalc/src/jEletroCalc/JEletroCalc.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jEletroCalc;
+
+import frms.EletroCalc;
+/**
+ *
+ * @author marcos-adm
+ */
+public class JEletroCalc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        EletroCalc fPlayer = new EletroCalc();
+        fPlayer.show();
+    }
+    
+}
